f9c5374a37ef943f2c3d45a37f731763e3b109ca
bpervan <branimir.pervan@gmail.com>
2015-06-21 14:34:37 +0200

    Added package and class Analyzer for Record analysis

---
+++ src/main/java/hr/bpervan/mt/analyzer/RecordAnalyzer.java
@@ -0,0 +1,23 @@
+package hr.bpervan.mt.analyzer;
+
+import hr.bpervan.mt.io.Record;
+
+import java.util.List;
+
+/**
+ * Created by Branimir on 21.6.2015..
+ */
+public class RecordAnalyzer {
+
+    public RecordAnalyzer(){
+
+    }
+
+    public void analyze(Record record){
+
+    }
+
+    public void analyze(List<Record> records){
+
+    }
+}
